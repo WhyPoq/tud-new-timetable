a0e9234305929bd140642ad4e3c2689c0654a635
rorzz <rorzz2@mail.ru>
2023-12-01 17:41:33 +0000

    first final version

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,51 @@
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.svg"
+
 const About = () => {
+    const feedbackFormLink = "https://docs.google.com/forms/d/e/1FAIpQLSdLiVd4DzUO2b4Unq-axHIdNO6PcGSd1xKC2U3YCienscIfqg/viewform?usp=sf_link"
+
     return ( 
-        <div>
-            Made by Konstantin Skliar
+        <div className="about-page">
+            <div className="inner-body">
+                <header>
+                    <div className="header-wrapper">
+                        <Link to="/" className="logo">
+                            <img src={logo} alt="Logo" />
+                        </Link>
+                        <h2>About</h2>
+                    </div>
+                </header>
+
+                <main>
+                    <div className="about-text-container">
+                        <p>
+                            I am a TUD student and I decided to build my version of the timetable website.
+                            Cause, beeing honest, the official one is awful.
+                        </p>
+
+                        <p>
+                            Now after you pick your timetable, your choice is saved. 
+                            And timetable properly updates every time you visit it.
+                        </p>
+
+                        <p>
+                            Site is now hosted on github pages, so it can't handle much traffic. 
+                            If there will be some demand for this site, I can host it properly.
+                        </p>
+
+                        <p>
+                            There may be some mistakes and bugs on some devices or for some programs.
+                            If there is an error, you can submit it in a feedback form (<Link to={ feedbackFormLink } target="_blank">form link</Link>)
+                            But I am not promising to fix it.
+                        </p>
+
+                        <p>Design is highly influenced by ITMO University timetable.</p>
+
+                        <p>Made by Konstantin Skliar</p>
+                    </div>
+                </main>
+
+            </div>
         </div>
     );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
 
 const Footer = () => {
+    const feedbackFormLink = "https://docs.google.com/forms/d/e/1FAIpQLSdLiVd4DzUO2b4Unq-axHIdNO6PcGSd1xKC2U3YCienscIfqg/viewform?usp=sf_link";
     return (
         <footer>
             <Link to="/about">About</Link>
-            <Link to="https://www.google.com/">Google</Link>
+            <Link to={ feedbackFormLink } target="_blank">Feedback</Link>
         </footer>
     );
 }
