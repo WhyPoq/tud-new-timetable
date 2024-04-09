169c2f7799b6cf93503b36eefc999eb1fab0fa20
rorzz <rorzz2@mail.ru>
2024-04-09 13:04:25 +0100

    deskto timeline fix

--- src/components/DesktopTimeLine.js
+++ src/components/DesktopTimeLine.js
@@ -4,33 +4,34 @@ import constants from "../constants";
 import moment from "moment";
 
 const DesktopTimeLine = ({ hourLen, fromTime, endTime }) => {
+	const [now, setNow] = useState(moment.utc().tz(constants.curTimezone));
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setNow(moment().utc().tz(constants.curTimezone));
+		}, 60 * 1000); // 60 * 1000 ms = 1 minute
 
-    const [now, setNow] = useState(moment.utc());
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setNow(moment().utc());
-        }, 60 * 1000); // 60 * 1000 ms = 1 minute
+		return () => clearInterval(interval); // Cleanup on component unmount
+	}, []);
 
-        return () => clearInterval(interval); // Cleanup on component unmount
-    }, []);
+	const hoursPassed =
+		now.clone().diff(now.clone().startOf("day")) *
+			constants.millisecondsToHours -
+		fromTime;
 
-    const hoursPassed = now.clone().diff(now.clone().startOf("day")) * constants.millisecondsToHours - fromTime;
-
-    return (
-        <div className="desktop-timeline-wrapper">
-            {
-                hoursPassed > 0 && hoursPassed < (endTime - fromTime) &&
-                <div
-                    className="desktop-timeline"
-                    style={{
-                        marginTop: hoursPassed * hourLen + "rem"
-                    }}
-                >
-                    <div></div>
-                </div>
-            }
-        </div>
-    );
-}
+	return (
+		<div className="desktop-timeline-wrapper">
+			{hoursPassed > 0 && hoursPassed < endTime - fromTime && (
+				<div
+					className="desktop-timeline"
+					style={{
+						marginTop: hoursPassed * hourLen + "rem",
+					}}
+				>
+					<div></div>
+				</div>
+			)}
+		</div>
+	);
+};
 
 export default DesktopTimeLine;
