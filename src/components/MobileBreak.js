b417917198ad261e5b40930a189f0404c64145a2
rorzz <rorzz2@mail.ru>
2024-04-11 10:27:14 +0100

    breaks fix

--- src/components/MobileBreak.js
+++ src/components/MobileBreak.js
@@ -1,27 +1,25 @@
+import constants from "../constants";
+
 const MobileBreak = ({ breakInfo, current }) => {
-    const fromStr = breakInfo.start.format("k:mm");
-    const toStr = breakInfo.end.format("k:mm");
+	const fromStr = breakInfo.start.tz(constants.curTimezone).format("k:mm");
+	const toStr = breakInfo.end.tz(constants.curTimezone).format("k:mm");
 
-    return (
-        <div
-            className={
-                "mobile-lesson mobile-break" + (current ? " current" : "")
-            }
-        >
-            {/* empty div to fit the grid */}
-            <div></div>
+	return (
+		<div className={"mobile-lesson mobile-break" + (current ? " current" : "")}>
+			{/* empty div to fit the grid */}
+			<div></div>
 
-            <div className="mobile-break-content">
-                <div className="mobile-break-line"></div>
-                <div>
-                    <p className="mobile-break-text">
-                        ...free from {fromStr} to {toStr}
-                    </p>
-                    <div className="mobile-break-text-underline"></div>
-                </div>
-            </div>
-        </div>
-    );
+			<div className="mobile-break-content">
+				<div className="mobile-break-line"></div>
+				<div>
+					<p className="mobile-break-text">
+						...free from {fromStr} to {toStr}
+					</p>
+					<div className="mobile-break-text-underline"></div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default MobileBreak;
